e4faf496fb2cb84d5a476a85d0e9ff4d345e6a15
Rasika Chavan <58884494+rasika-chavan@users.noreply.github.com>
2020-10-09 22:01:54 +0530

    Updated signup fully working

--- loginsystem/add.php
+++ loginsystem/add.php
@@ -1,13 +1,12 @@
 <?php
 session_start();
 require_once "pdo.php";
-// require_once "layout.php";
+require_once "layout.php";
 if(isset($_POST['cancel'])) // user submits CANCEL button
 {
     header('Location: index.php');
     return;
 }
-
 else if(isset($_POST['add'])) // user submits ADD button
 {
     if (strlen($_POST['make']) < 1 || strlen($_POST['model']) < 1 || strlen($_POST['year']) < 1 || strlen($_POST['mileage']) < 1) 
@@ -67,10 +66,9 @@ else if(isset($_POST['add'])) // user submits ADD button
 ?>
 <!DOCTYPE html>
 <html>
-
 <head>
 	<meta charset="utf-8">
-	<title>Registration</title>
+	<title>Sign Up</title>
 	<!-- Mobile Specific Metas -->
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 	<!-- Font-->
@@ -82,7 +80,6 @@ else if(isset($_POST['add'])) // user submits ADD button
 	<link rel="stylesheet" href="css/stylenavbar.css">
     <script src="css/navbar.js"></script>
 </head>
-
 <body class="form-v10" >
 	<div class="page-content" style="background: -moz-linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 35%, rgba(0,181,255,1) 100%);
 background: -webkit-linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 35%, rgba(0,181,255,1) 100%);
@@ -90,19 +87,16 @@ background: linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 35%, rgba(
 		<div class="form-v10-content">
 			<form class="form-detail" method="post" enctype="multipart/form-data">
 				<div class="form-right">
-                <?php
-    if (isset($_SESSION['error'])) 
-    {
-        echo('<p style="color: red; position:relative; left:50px; top:10px;">' . htmlentities($_SESSION['error']) . "</p>\n");
-        unset($_SESSION['error']);
-    }
-    ?>
-					<h2>Details</h2>
-					<div class="form-group">
-						<div class="form-row form-row-1">
-							<input type="text" name="make" class="contactno" id="no" placeholder="Username"
-								  >
-						</div>
+                    <h2>SIGN UP</h2>
+                    <?php
+                        if (isset($_SESSION['error'])) 
+                        {
+                            echo('<p style="color: red; position:relative; left:50px; top:10px;">' . htmlentities($_SESSION['error']) . "</p>\n");
+                            unset($_SESSION['error']);
+                        }
+                    ?>			
+					<div class="form-row">
+						<input type="text" name="make" class="contactno" id="no" placeholder="Username">
 					</div>
 					<div class="form-row">
 						<input type="text" name="model" id="your_email" class="input-text"   
@@ -114,30 +108,6 @@ background: linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 35%, rgba(
 					<div class="form-row">
 						<input type="password" name="mileage" class="answer" id="password" placeholder="Re-Enter Password">
 					</div>
-					<!-- <div class="form-row">
-						<select name="security">
-						<option class="option" value="question">Select A Seurity Question</option>
-							<option class="option" value="What is the name of your pet?">What is the name of your pet?</option>
-							<option class="option" value="Which school did you attend?">Which school did you attend?</option>
-							<option class="option" value="What is your mother's name?">What is your mother's name?</option>
-							<option class="option" value="In what city were you born?">In what city were you born?</option>
-							<option class="option" value="What was your childhood nickname?">What was your childhood nickname?</option>
-						</select>
-						<span class="select-btn">
-							<i class="zmdi zmdi-chevron-down"></i>
-						</span>
-					</div> -->
-					<!-- <div class="form-row">
-						<input type="text" name="answer" class="answer" id="answer" placeholder="Answer"   >
-					</div> -->
-					<!-- <div class="form-checkbox">
-						<label class="container">
-							<p>I do accept the <a href="#" class="text">Terms and Conditions</a> of your site.</p>
-							<input type="checkbox" name="checkbox" checked="checked"   >
-							<span class="checkmark"></span>
-									
-						</label>
-					</div> -->
 					<div class="form-row-last">
 						<input type="submit" name="add" class="register" value="Register">
                         <input type="submit" name="cancel" class="register" value="Cancel">
@@ -148,44 +118,6 @@ background: linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 35%, rgba(
 	</div>
 </body>
 </html>
-<!-- <!DOCTYPE html>
-<html>
-<head>
-    <title>Sign Up</title>
-</head>
-<body>
-<center>
-<div class="container">
-    <h1>SIGN UP</h1>
-    <?php
-    if (isset($_SESSION['error'])) 
-    {
-        echo('<p style="color: red;">' . htmlentities($_SESSION['error']) . "</p>\n");
-        unset($_SESSION['error']);
-    }
-    ?>
-
-    <form method="post">
-        <p>Name:
-        <input type="text" name="make" size="40"/></p>
-
-        <p>Email:
-        <input type="text" name="model" size="40"/></p>
-
-        <p>Password:
-        <input type="password" name="year" size="40"/></p>
-
-        <p>Confirm Password:
-        <input type="password" name="mileage" size="40"/></p>
-        <input type="submit" name="add" value="Sign Up">
-        <input type="submit" name="cancel" value="Cancel">
-    </form>
-    <br><br>
-</div>
-Already Have an account? <a href="login.php">Log in here</a></center>
-</body>
-</html>-->
 <?php
-// echo "<br>";
 require_once "footer.php";
 ?> 
